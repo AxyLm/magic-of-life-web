1752b44e8f3ace473db913cb4edcd46a6f9de7b1
wangyuyang <thelmg@163.com>
2020-10-22 18:31:19 +0800

    增加端口占用检查

--- start.js
+++ start.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const db = require('./db/connect')
 const app = express()
+const net = require('net')
 const path = require('path')
 const cors = require('cors')
 const colors = require('colors');
@@ -34,8 +35,16 @@ app.use('/monit',api)
 app.use('/',express.static(path.join(__dirname,'./static/apidoc'))) // 接口文档页
 app.use('/public',express.static(path.join(__dirname,'./static/media'))) // 静态目录
 
-
-app.listen(SERVER_PORT,()=>{
-  log.info('[app] start success')
-  console.log(colors.green('[app] start success：http://localhost:'+SERVER_PORT))
+let server = net.createServer().listen(SERVER_PORT)
+server.on('listening', function () {
+  server.close()
+  app.listen(SERVER_PORT, () => {
+    log.info('[app] start success')
+    console.log(colors.green('[app] start success：http://localhost:' + SERVER_PORT))
+  })
+})
+server.on('error', function (err) {
+  if (err.code === 'EADDRINUSE') {
+    log.error('[app] 端口被占用',SERVER_PORT)
+  }
 })
